6261290c1072961ae24044cb64ad273e245cd39d
partht555 <partht555@gmail.com>
2025-07-13 11:29:23 -0500

    Initial commit

---
+++ trend_spotter/agent.py
@@ -0,0 +1,21 @@
+# trend_spotter/agent.py
+from google.adk.agents import LlmAgent
+from google.adk.tools.agent_tool import AgentTool
+# Import the sub-agent INSTANCES
+from .sub_agents.google_search_agent import google_search_agent
+from .sub_agents.reddit_agent import reddit_agent
+from . import prompt
+#MODEL = "gemini-2.5-pro-preview-05-06"
+MODEL = "gemini-1.5-flash"
+# This is our main "manager" agent, now an LlmAgent
+root_agent = LlmAgent(
+model=MODEL,
+name="TrendSpotterOrchestrator",
+description="The manager of a team of specialist AI agents.",
+instruction=prompt.ORCHESTRATOR_PROMPT,
+# The Orchestrator's "tools" are its sub-agents, wrapped in AgentTool
+tools=[
+AgentTool(agent=google_search_agent),
+AgentTool(agent=reddit_agent)
+],
+)
---
+++ trend_spotter/envs.py
@@ -0,0 +1,7 @@
+import os
+
+# Set your Google API Key (for Google AI API)
+os.environ["GOOGLE_API_KEY"] = "AIzaSyA9usR_sRUtDZFQnp_wZOl9KzqVH9HgVKs"
+
+
+print("Environment variables set for this session.")
